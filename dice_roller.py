2970f22bc366810c9231ff17e78043433148fb99
Houdini <nonsoamadi@aol.com>
2020-07-19 00:31:40 +0100

    rolled a dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,15 @@
-def main():
-  print('You rolled a die')
+import random
 
-if __name__== "__main__":
-  main()
+
+def main():
+    print('You rolled a die')
+
+    roll = random.randint(1, 6)
+    print(f'You rolled a {roll}')
+
+    # You rolled a die
+  # You rolled a 6
+
+
+if __name__ == "__main__":
+    main()
